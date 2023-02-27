d8e8b6a00206a2650bc9d73e8d347c290246c6e0
Isaias Moresco <contato@isaiasmores.co>
2023-02-26 22:14:43 -0300

    Normalização das informações, e melhorias na exibição do resultado

--- A3S/js/calculadora.js
+++ A3S/js/calculadora.js
@@ -3,19 +3,48 @@ import subtrair from "./subtrair.js";
 import multiplicar from "./multiplicar.js";
 import dividir from "./dividir.js";
 
-let n1 = parseFloat(prompt("Informe o primeiro numero:"));
-let n2 = parseFloat(prompt("Informe o segundo numero:"));
+var n1, n2;
 
-console.log(somar(n1, n2));
-console.log(subtrair(n1, n2));
-console.log(multiplicar(n1, n2));
-console.log(dividir(n1, n2));
+function solicitarNumeros() {
+ n1 = parseFloat(prompt("Informe o primeiro numero:"));
+  console.log(n1);
+ n2 = parseFloat(prompt("Informe o segundo numero:"));
+  console.log(n2);
+  isNaNVerification(n1, n2);
+  calcular(n1, n2);
+}
 
-/*window.alert(
-  `Soma: ${somar(n1, n2)}\nSubtração: ${subtrair(
+
+
+function calcular(n1, n2) {
+  document.querySelector("h1").innerHTML = "";
+  document.querySelector("h1").innerText += `Soma de ${n1} + ${n2} = ${somar(
     n1,
     n2
-  )}\nMultiplicação: ${multiplicar(n1, n2)}\nDivisão: ${dividir(n1, n2)}\n`
-);*/
+  )}\nSubtração de ${n1} - ${n2} = ${subtrair(
+    n1,
+    n2
+  )}\nMultiplicação de ${n1} * ${n2} = ${multiplicar(
+    n1,
+    n2
+  )}\nDivisão de ${n1} / ${n2} = ${dividir(n1, n2)}\n`;
+}
 
-document.querySelector("h1").innerText += `Soma de ${n1} + ${n2} = ${somar(n1,n2)}\nSubtração de ${n1} - ${n2} = ${subtrair(n1, n2)}\nMultiplicação de ${n1} * ${n2} = ${multiplicar(n1,n2)}\nDivisão de ${n1} / ${n2}: ${dividir(n1, n2)}\n`;
+function isNaNVerification(n1, n2){
+  if (isNaN(n1) || isNaN(n2)){
+window.alert("Insira um número por favor!")
+solicitarNumeros();
+  }
+}
+
+function limparResultado(){
+  document.querySelector("h1").innerHTML = `0`;
+}
+
+
+var button = document.getElementById("newCalc");
+button.addEventListener("click", limparResultado);
+button.addEventListener("click", solicitarNumeros);
+
+
+solicitarNumeros();
