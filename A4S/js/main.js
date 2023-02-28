040d960fdc6456e6c4d13897f0d24830f225db10
Isaias Moresco <contato@isaiasmores.co>
2023-02-28 00:23:56 -0300

    Percorrendo o DOM

---
+++ A4S/js/main.js
@@ -0,0 +1,109 @@
+
+
+// Selecionando o body
+
+let body = document.querySelector('body')
+console.log(body)
+
+
+//Selecionando o body>h1
+
+let h1 = document.querySelector('h1')
+console.log(h1)
+
+
+//Selecionando a body>div class="conteudo"
+
+let conteudo = document.querySelector('.conteudo')
+console.log(conteudo)
+
+//Selecionando o body>h5
+
+let h5 = document.querySelector('h5')
+console.log(h5)
+
+//Selecionando body>div>ul
+let ul = document.querySelector('ul')
+console.log(ul)
+
+let li1 = document.getElementById('item1')
+console.log(li1)
+let li2 = document.getElementById('item2')
+console.log(li2)
+let li3 = document.getElementById('item3')
+console.log(li3)
+
+//Selecionando body>section
+
+let section = document.querySelector('section')
+console.log(section)
+
+//Selecionando todos os articles dentro de body>section
+
+let articles = document.querySelectorAll('article')
+console.log(articles)
+
+
+//Selecionando body>section>article id="card1"
+
+let card1 = document.getElementById('card1')
+console.log(card1)
+
+//Selecionando body>section>article id="card1">h2 id="titleCard1"
+
+let titleCard1 = document.getElementById('titleCard1')
+console.log(titleCard1)
+
+
+//Selecionando body>section>article id="card1">p
+
+let p1 = document.querySelector('section article p')
+console.log(p1)
+
+
+//Selecionando body>section>article class="animado"
+
+let animado = document.querySelector('.animado')
+console.log(animado)
+
+//Selecionando body>section>article class="animado">h2
+
+let animadoH2 = document.querySelector('.animado h2')
+console.log(animadoH2)
+
+//Selecionando body>section>article class="animado">p
+
+let animadoP = document.querySelector('.animado p')
+console.log(animadoP)
+
+
+//Selecionando body>section>article class="diferente"
+
+let diferente = document.querySelector('.diferente')
+console.log(diferente)
+
+//Selecionando body>section>article class="diferente">h2
+
+let diferenteH2 = document.querySelector('.diferente h2')
+console.log(diferenteH2)
+
+//Selecionando body>section>article class="diferente">p
+
+let diferenteP = document.querySelector('.diferente p')
+console.log(diferenteP)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
