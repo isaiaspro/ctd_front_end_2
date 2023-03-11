b529e3f019b3629b8580f1c96c9f0eb36381f66a
Isaias Moresco <contato@isaiasmores.co>
2023-03-11 17:12:17 -0300

    inicio do desenvolvimento das funções de validação do formulário

--- Checkpoint1/main.js
+++ Checkpoint1/main.js
@@ -1,16 +1,34 @@
-// Definir uma função para gerar uma mensagem sobre o Dia Internacional da Mulher
-function gerarMensagem() {
-    const palavras1 = ["Celebre", "Honre", "Lembre-se", "Reconheça"];
-    const palavras2 = ["a força", "a coragem", "a determinação", "a resiliência"];
-    const palavras3 = ["das mulheres", "das mães", "das filhas", "das avós"];
-  
-    const rand1 = Math.floor(Math.random() * palavras1.length);
-    const rand2 = Math.floor(Math.random() * palavras2.length);
-    const rand3 = Math.floor(Math.random() * palavras3.length);
-  
-    return `${palavras1[rand1]} ${palavras2[rand2]} ${palavras3[rand3]} neste Dia Internacional da Mulher!`;
-  }
-  
-  // Exibir a mensagem gerada pela função
-  console.log(gerarMensagem());
-  
+function validaTitulo(){
+  let errorMessageRef = document.querySelector('.input_titulo') 
+  let taskNameRef = document.querySelector('#taskName')
+  let errorMessage = document.createElement('p')
+  errorMessage.id = 'taskErrorMessage'
+  errorMessage.innerText = 'Por favor, preencha corretamente o campo Título'
+  errorMessageRef.appendChild(errorMessage)
+  taskNameRef.classList.add('error')
+}
+
+function validaUrl(){
+  let errorMessageRef = document.querySelector('.input_url') 
+  let urlRef = document.querySelector('#url')
+  let errorMessage = document.createElement('p')
+  errorMessage.id = 'taskErrorMessage'
+  errorMessage.innerText = 'Por favor, preencha corretamente o campo URL'
+  errorMessageRef.appendChild(errorMessage)
+  urlRef.classList.add('error')
+}
+
+function validaDescricao(){
+  let errorMessageRef = document.querySelector('.input_descricao') 
+  let descricaoRef = document.querySelector('#description')
+  let errorMessage = document.createElement('p')
+  errorMessage.id = 'taskErrorMessage'
+  errorMessage.innerText = 'Por favor, preencha corretamente o campo Descrição'
+  errorMessageRef.appendChild(errorMessage)
+  descricaoRef.classList.add('error')
+}
+
+validaTitulo()
+validaUrl()
+validaDescricao()
+
