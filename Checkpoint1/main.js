104a651631bad83a7e5ddc7eef15906c4700b900
Isaias Moresco <contato@isaiasmores.co>
2023-03-12 09:31:06 -0300

    trabalhando nas validações do form

--- Checkpoint1/main.js
+++ Checkpoint1/main.js
@@ -1,34 +1,83 @@
-function validaTitulo(){
+const taskNameRef = document.querySelector('#taskName')
+const urlRef = document.querySelector('#url')
+const descriptionRef = document.querySelector('#description')
+const createTaskButtonRef = document.querySelector('#createTaskButton')
+
+console.log(taskNameRef)
+console.log(urlRef)
+console.log(descriptionRef)
+console.log(createTaskButtonRef)
+
+
+
+var formTask = {
+  titulo: '',
+  url: '',
+  descricao: ''
+}
+
+
+// console.log(formTask)
+
+function validaTitulo(titulo){
   let errorMessageRef = document.querySelector('.input_titulo') 
   let taskNameRef = document.querySelector('#taskName')
   let errorMessage = document.createElement('p')
+
+  if(!errorMessage){
   errorMessage.id = 'taskErrorMessage'
   errorMessage.innerText = 'Por favor, preencha corretamente o campo Título'
   errorMessageRef.appendChild(errorMessage)
-  taskNameRef.classList.add('error')
+}
+taskNameRef.classList.add('error')
+formTask.titulo = titulo
 }
 
-function validaUrl(){
+
+function validaUrl(url){
   let errorMessageRef = document.querySelector('.input_url') 
   let urlRef = document.querySelector('#url')
   let errorMessage = document.createElement('p')
+
+  if(!errorMessage){
   errorMessage.id = 'taskErrorMessage'
   errorMessage.innerText = 'Por favor, preencha corretamente o campo URL'
   errorMessageRef.appendChild(errorMessage)
-  urlRef.classList.add('error')
+}
+urlRef.classList.add('error')
+formTask.url = url
 }
 
-function validaDescricao(){
+
+
+function validaDescricao(descricao){
   let errorMessageRef = document.querySelector('.input_descricao') 
   let descricaoRef = document.querySelector('#description')
   let errorMessage = document.createElement('p')
+
+  if(!errorMessage){
   errorMessage.id = 'taskErrorMessage'
   errorMessage.innerText = 'Por favor, preencha corretamente o campo Descrição'
   errorMessageRef.appendChild(errorMessage)
-  descricaoRef.classList.add('error')
+}
+descricaoRef.classList.add('error')
+formTask.descricao = descricao
 }
 
-validaTitulo()
-validaUrl()
-validaDescricao()
+function newTask(event){
+  event.preventDefault()
+  console.log(formTask)
+}
+
+taskNameRef.addEventListener('keyup', (event) => validaTitulo(event.target.value))
+urlRef.addEventListener('keyup', (event) => validaUrl(event.target.value))
+descriptionRef.addEventListener('keyup', (event) => validaDescricao(event.target.value))
+
+
+createTaskButtonRef.addEventListener('click', (event) => newTask(event))
+
+
+// validaTitulo()
+// validaUrl()
+// validaDescricao()
 
